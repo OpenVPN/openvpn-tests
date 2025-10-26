8bee12742d79b33c0c94c204da2512ae717a2fde
Samuli Seppänen <sasepp@pm.me>
2025-10-26 09:42:30 +0100

    tserver: parameterize run_t_clients.sh
    Signed-off-by: Samuli Seppänen <sasepp@pm.me>

--- t_server/original/run_t_clients.sh
+++ t_server/original/run_t_clients.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 #
-KEY=/home/rocky/.ssh/id_ed25519
-HOST=tserver-client.foss.vpn
+. /var/lib/provision/deployment-config.sh
+
+KEY=$SSH_PRIVATE_KEY
+HOST=tserver-client.$PRIVATE_DNS_ZONE_NAME
 #TESTSETS="22 23.small 23 24 25 26 master"
 TESTSETS="master"
 
-LOGDIR=/home/rocky/t_server_logs
+LOGDIR=$HOMEDIR/t_server_logs
 DAY=`date +%Y%m%d`
 NOW=`date +%Y%m%d-%H%M`
 
