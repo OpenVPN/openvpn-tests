3946e302c8a3354f510cb08318a3fcaab88ee8c5
Samuli Seppänen <sasepp@pm.me>
2025-11-11 13:07:20 +0200

    tserver tofu: create 2.7 and master containers
    Signed-off-by: Samuli Seppänen <sasepp@pm.me>

--- t_server/tofu/modules/tserver_user_data/provision/92-setup-old-openvpn-versions.sh
+++ t_server/tofu/modules/tserver_user_data/provision/92-setup-old-openvpn-versions.sh
@@ -7,4 +7,4 @@ set -e
 cd /var/lib/provision/podman
 podman build --cap-add=CAP_NET_ADMIN --cap-add=CAP_MKNOD -f Containerfile.ubuntu-14.04 -t openvpn-22 -t openvpn-23 .
 podman build --cap-add=CAP_NET_ADMIN --cap-add=CAP_MKNOD -f Containerfile.ubuntu-20.04 -t openvpn-24 -t openvpn-25 .
-podman build --cap-add=CAP_NET_ADMIN --cap-add=CAP_MKNOD -f Containerfile.ubuntu-24.04 -t openvpn-26 -t openvpn-27 .
+podman build --cap-add=CAP_NET_ADMIN --cap-add=CAP_MKNOD -f Containerfile.ubuntu-24.04 -t openvpn-26 -t openvpn-27 -t openvpn-master .
